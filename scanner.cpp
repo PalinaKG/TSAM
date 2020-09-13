f8f82cc4342acb021837c74ff54e90090e00999f
jonasmark <jonasmar14@gmail.com>
2020-09-13 17:22:28 +0000

    aa

--- scanner.cpp
+++ scanner.cpp
@@ -1,10 +1,10 @@
-//#include <sys/socket.h>
+#include <sys/socket.h>
 // #include<stdio.h>	//for printf
 //#include<string.h> //memset
 // #include<stdlib.h> //for exit(0);
 // #include<errno.h> //For errno - the error number
 #include<netinet/udp.h>	//Provides declarations for udp header
-//#include<netinet/ip.h>	//Provides declarations for ip header
+#include<netinet/ip.h>	//Provides declarations for ip header
 
 #include <stdio.h>	//for printf
 #include <string.h> //memset
@@ -17,31 +17,36 @@
 #include <unistd.h> // sleep()
 #include <typeinfo>
 //https://www.binarytides.com/raw-sockets-c-code-linux/
-http://www.cis.syr.edu/~wedu/seed/Labs_12.04/Networking/DNS_Remote/udp.c 
+// http://www.cis.syr.edu/~wedu/seed/Labs_12.04/Networking/DNS_Remote/udp.c 
 
 
-// struct ipheader {
-//     //IP header format
-//     unsigned char      iph_ihl:4; //IHL
-//     unsigned char      iph_ver:4; //Version
-//     unsigned char      iph_tos; //Type of service
-//     unsigned short     iph_len; //Total length
-//     unsigned short     iph_ident; //Identification
-//     unsigned short     iph_offset; //Fragment offset
-//     unsigned char      iph_ttl; //Time to live
-//     unsigned char      iph_protocol; //Protocol
-//     unsigned short     iph_csum; //Checksum
-//     unsigned int       iph_source; //Source address
-//     unsigned int       iph_dest; //Destination address
+// my adding imports 
+#include <sys/types.h>
+#include <sys/time.h>
 
-// };
 
-// struct udpheader {
-//     unsigned short      udph_srcport; //Source port (2 bytes)
-//     unsigned short      udph_destport; //Destination port (2 bytes)
-//     unsigned short      udph_len; //UDP length (2 bytes)
-//     unsigned short      udph_csum; //UDP checksum (2 bytes)
-// };
+
+struct ipheader {
+    //IP header format
+    unsigned char      iph_ihl:4; //IHL
+    unsigned char      iph_ver:4; //Version
+    unsigned char      iph_tos; //Type of service
+    unsigned short     iph_len; //Total length
+    unsigned short     iph_ident; //Identification
+    unsigned short     iph_offset; //Fragment offset
+    unsigned char      iph_ttl; //Time to live
+    unsigned char      iph_protocol; //Protocol
+    unsigned short     iph_csum; //Checksum
+    unsigned int       iph_source; //Source address
+    unsigned int       iph_dest; //Destination address
+};  
+
+struct udpheader {
+    unsigned short      udph_srcport; //Source port (2 bytes)
+    unsigned short      udph_destport; //Destination port (2 bytes)
+    unsigned short      udph_len; //UDP length (2 bytes)
+    unsigned short      udph_csum; //UDP checksum (2 bytes)
+};
 
 unsigned short csum(unsigned short *ptr, int nbytes) {
     register long sum;
@@ -75,106 +80,154 @@ struct pseudo_header
 	u_int16_t tcp_length;
 };
 
-int main () {
-    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); //raw UDP socket created using the socket function
-    //std::string ip="127.0.0.1";
-   //Create a raw socket
-	int s = socket (PF_INET, SOCK_RAW, IPPROTO_TCP);
-	
-	if(s == -1)
-	{
-		//socket creation failed, may be because of non-root privileges
-		perror("Failed to create socket");
-		exit(1);
+
+
+////// main ////////
+int main (int argc, char* argv[]) {
+
+	if (argc != 4){
+		printf("Usage: scanner <ip address> <low port> <high port> \n");
+        exit(0);
 	}
-	
+    
 	//Datagram to represent the packet
 	char datagram[512] , source_ip[32] , *data , *pseudogram;
 	
 	//zero out the packet buffer
 	memset (datagram, 0, 512);
 	
-	//IP header
-	struct ip *iph = (struct ip *) datagram;
-
-    //TCP header
-	struct udphdr *udph = (struct udphdr *) (datagram + sizeof (struct ip));
+    //UDP header
+	struct udpheader *udph = (struct udpheader *) (datagram + sizeof (struct ipheader));
 	struct sockaddr_in sin;
 	struct pseudo_header psh;
 
     //Data part
-	data = datagram + sizeof(struct ip) + sizeof(struct udphdr);
-	strcpy(data , "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+	data = datagram + sizeof(struct ipheader) + sizeof(struct udpheader);
+	strcpy(data , "Are you open?");
 	
 	//some address resolution
-	strcpy(source_ip , "192.168.1.1");
+	strcpy(source_ip , "192.168.1.12");
 	sin.sin_family = AF_INET;
-	sin.sin_port = htons(80);
-	sin.sin_addr.s_addr = inet_addr ("130.208.243.61");
- 
+	sin.sin_port = htons(4000);
+	// sin.sin_addr.s_addr = inet_addr ("130.208.243.61");
+	sin.sin_addr.s_addr = inet_addr (argv[1]);
 
-	struct in_addr * src_add = (struct in_addr *) inet_addr (source_ip);
-	iph->ip_src=src_add;
-    iph->ip_tos = 0; //Type of service
-    iph->ip_len = sizeof (struct ip) + sizeof (struct udphdr) + strlen(data); //Total length
-    iph->ip_id = htonl(11); //Identification er 16 bitar þannig kannski nota htnos??
-    iph->ip_off = 0; //Fragment offset
-    iph->ip_ttl = 255; //Maximum number (perhaps to large?), often recommended to use minimum of 64
-    iph->ip_p = IPPROTO_UDP; //Protocol
-	//typeid(iph->ip_src).name();
-	//typeid(iph->ip_dst)=typeid(inet_addr ("130.208.243.61"));
-	//ip_dst=inet_addr ("130.208.243.61");
-  //iph->ip_src = inet_addr (source_ip); //Checksum
-    //iph->ip_dst = inet_addr ("130.208.243.61");  //Destination address
+
+	// Creating ip header
+	struct ipheader *iph = (struct ipheader *) datagram;
+     
+    iph->iph_tos = 0; //Type of service
+    iph->iph_len = sizeof (struct ipheader) + sizeof (struct udpheader) + strlen(data);  //Total length
+    iph->iph_ident = htonl(11); //Identification
+    iph->iph_offset = 0; //Fragment offset
+    iph->iph_ttl = 255; //Time to live
+    iph->iph_protocol = IPPROTO_UDP; //Protocol
+    
+    iph->iph_source = inet_addr (source_ip); //Source address
+    // iph->iph_dest = inet_addr ("130.208.243.61"); //Destination address
+	iph->iph_dest = inet_addr (argv[1]); //Destination address
 
 
    //Ip checksum
-	iph->check = sum ((unsigned short *) datagram, iph->iph_len);
+	iph->iph_csum = csum ((unsigned short *) datagram, iph->iph_len);
+
+    // UDP header
+    int size_udph = strlen(datagram) + sizeof (struct ipheader);
 
     udph->udph_srcport = htons(0);
-    udph->udph_destport = htons("4007");
-    udph->udph_len = sizeof (struct udphr) + strlen(data);
+    udph->udph_destport = htons(4000);
+    udph->udph_len = size_udph + strlen(data);
     udph->udph_csum = 0;
 
-    //Now the TCP checksum
+    
+    //Now the UDP checksum
 	psh.source_address = inet_addr( source_ip );
 	psh.dest_address = sin.sin_addr.s_addr;
 	psh.placeholder = 0;
 	psh.protocol = IPPROTO_UDP;
-	psh.tcp_length = htons(sizeof(struct udphdr) + strlen(data) );
+	psh.tcp_length = htons(size_udph + strlen(data) );
 	
-	int psize = sizeof(struct pseudo_header) + sizeof(struct udphdr) + strlen(data);
-	pseudogram = malloc(psize);
+	int psize = sizeof(struct pseudo_header) + size_udph + strlen(data);
+	pseudogram = (char *) malloc(psize);
 	
 	memcpy(pseudogram , (char*) &psh , sizeof (struct pseudo_header));
-	memcpy(pseudogram + sizeof(struct pseudo_header) , tcph , sizeof(struct udphdr) + strlen(data));
+	memcpy(pseudogram + sizeof(struct pseudo_header) , udph , size_udph + strlen(data));
 	
-	udp->udph_csum = csum( (unsigned short*) pseudogram , psize);
+	udph->udph_csum = csum( (unsigned short*) pseudogram , psize);
 	
-    //IP_HDRINCL to tell the kernel that headers are included in the packet kannski óþarfi??
-	int one = 1;
-	const int *val = &one;
+
+    //Create a raw UDP socket
+    int s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); 
 	
-	if (setsockopt (s, IPPROTO_IP, IP_HDRINCL, val, sizeof (one)) < 0)
+	if(s== -1)
 	{
-		perror("Error setting IP_HDRINCL");
+		//socket creation failed, may be because of non-root privileges
+		perror("Failed to create socket");
+		exit(1);
+	}
+
+	
+	// http://www.microhowto.info/howto/listen_for_and_receive_udp_datagrams_in_c.html
+	char buffer[512];
+	struct sockaddr_storage src_addr;
+
+	struct iovec iov[1];
+	iov[0].iov_base=buffer;
+	iov[0].iov_len=sizeof(buffer);
+
+	struct msghdr message;
+	message.msg_name=&src_addr;
+	message.msg_namelen=sizeof(src_addr);
+	message.msg_iov=iov;
+	message.msg_iovlen=1;
+	message.msg_control=0;
+	message.msg_controllen=0;
+
+
+    // initializing timeout for socket
+	// https://stackoverflow.com/questions/13547721/udp-socket-set-timeout
+	struct timeval tv;
+	tv.tv_sec = 2;
+	tv.tv_usec = 0;	
+	if (setsockopt (s, SOL_SOCKET, SO_RCVTIMEO ,&tv,sizeof(tv)) < 0)
+	{
+		perror("Error socket setup options");
 		exit(0);
 	}
 
-    while (1)
-	{
+	int i = atoi(argv[2]);
+	int i_stop = atoi(argv[3])+1;
+	while (i<i_stop){
+		sin.sin_port = htons(i);
+
 		//Send the packet
-		if (sendto (sock, datagram, udph->udph_len ,	0, (struct sockaddr *) &sin, sizeof (sin)) < 0)
+		if (sendto (s, data, udph->udph_len , 0, (struct sockaddr *) &sin, sizeof (sin)) < 0)
 		{
 			perror("sendto failed");
 		}
 		//Data send successfully
 		else
 		{
-			printf ("Packet Send. Length : %d \n" , udph->udph_len);
+			printf ("Packet Sent to port %d \n" , i); 
 		}
-        // sleep for 1 seconds
-        sleep(1);
-	}
+
+		ssize_t count=recvmsg(s,&message,0);
+		
+		if (count==-1) {
+			printf("%s",strerror(errno));
+		} else if (message.msg_flags&MSG_TRUNC) {
+			printf("datagram too large for buffer: truncated");
+		} else {
+			printf("%s",buffer);
 	
+		}
+
+		printf("\n\n");
+
+		i++; 
+
+	} 
+
+
 	return 0;
+}
