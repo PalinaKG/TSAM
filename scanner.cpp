710ef275180f4a626cd694788c5091f8696ddbdf
jonasmark <jonasmar14@gmail.com>
2020-09-17 17:57:01 +0000

    scanner v2

--- scanner.cpp
+++ scanner.cpp
@@ -1,179 +1,47 @@
-#include <sys/socket.h>
-// #include<stdio.h>	//for printf
-//#include<string.h> //memset
-// #include<stdlib.h> //for exit(0);
-// #include<errno.h> //For errno - the error number
-#include<netinet/udp.h>	//Provides declarations for udp header
-#include<netinet/ip.h>	//Provides declarations for ip header
+#include <stdio.h>       // for printf
+#include <stdlib.h>      // for exit()
+#include <string.h>      // for strlen()
 
-#include <stdio.h>	//for printf
-#include <string.h> //memset
-#include <sys/socket.h>	//for socket ofcourse
-#include <stdlib.h> //for exit(0);
-#include <errno.h> //For errno - the error number
-// #include <netinet/tcp.h>	//Provides declarations for tcp header
-#include <netinet/ip.h>	//Provides declarations for ip header
-#include <arpa/inet.h> // inet_addr
-#include <unistd.h> // sleep()
-#include <typeinfo>
-//https://www.binarytides.com/raw-sockets-c-code-linux/
-// http://www.cis.syr.edu/~wedu/seed/Labs_12.04/Networking/DNS_Remote/udp.c 
+#include <sys/socket.h>  // for udp socket
+#include <netinet/in.h>  // --//--
+#include <netinet/udp.h> // --//--
+#include <sys/types.h>   // recvfrom
 
+#include <arpa/inet.h>   // inet_addr
 
-// my adding imports 
-#include <sys/types.h>
-#include <sys/time.h>
+#include <vector> 
+#include <iostream>
 
+void arr_resize(int* & arr );
+bool not_in_arr( int * arr, int n_foundports ,int port_poss);
+void print_arr(int * arr, int num_ports);
+void print_msg(char* buf, unsigned int msg_size);
 
+int main(int argc, char* argv[]){
 
-struct ipheader {
-    //IP header format
-    unsigned char      iph_ihl:4; //IHL
-    unsigned char      iph_ver:4; //Version
-    unsigned char      iph_tos; //Type of service
-    unsigned short     iph_len; //Total length
-    unsigned short     iph_ident; //Identification
-    unsigned short     iph_offset; //Fragment offset
-    unsigned char      iph_ttl; //Time to live
-    unsigned char      iph_protocol; //Protocol
-    unsigned short     iph_csum; //Checksum
-    unsigned int       iph_source; //Source address
-    unsigned int       iph_dest; //Destination address
-};  
-
-struct udpheader {
-    unsigned short      udph_srcport; //Source port (2 bytes)
-    unsigned short      udph_destport; //Destination port (2 bytes)
-    unsigned short      udph_len; //UDP length (2 bytes)
-    unsigned short      udph_csum; //UDP checksum (2 bytes)
-};
-
-unsigned short csum(unsigned short *ptr, int nbytes) {
-    register long sum;
-	unsigned short oddbyte;
-	register short answer;
-
-	sum=0;
-	while(nbytes>1) {
-		sum+=*ptr++;
-		nbytes-=2;
-	}
-	if(nbytes==1) {
-		oddbyte=0;
-		*((u_char*)&oddbyte)=*(u_char*)ptr;
-		sum+=oddbyte;
-	}
-
-	sum = (sum>>16)+(sum & 0xffff);
-	sum = sum + (sum>>16);
-	answer=(short)~sum;
-	
-	return(answer);
-}
-
-struct pseudo_header
-{
-	u_int32_t source_address;
-	u_int32_t dest_address;
-	u_int8_t placeholder;
-	u_int8_t protocol;
-	u_int16_t tcp_length;
-};
-
-
-
-////// main ////////
-int main (int argc, char* argv[]) {
-
-	if (argc != 4){
+    if (argc != 4){
 		printf("Usage: scanner <ip address> <low port> <high port> \n");
         exit(0);
 	}
-    
-	//Datagram to represent the packet
-	char datagram[512] , source_ip[32] , *data , *pseudogram;
-	
-	//zero out the packet buffer
-	memset (datagram, 0, 512);
-	
-    //UDP header
-	struct udpheader *udph = (struct udpheader *) (datagram + sizeof (struct ipheader));
-	struct sockaddr_in sin;
-	struct pseudo_header psh;
 
-    //Data part
-	data = datagram + sizeof(struct ipheader) + sizeof(struct udpheader);
-	strcpy(data , "Are you open?");
-	
-	//some address resolution
-	strcpy(source_ip , "192.168.1.12");
-	sin.sin_family = AF_INET;
-	sin.sin_port = htons(4000);
-	// sin.sin_addr.s_addr = inet_addr ("130.208.243.61");
-	sin.sin_addr.s_addr = inet_addr (argv[1]);
+    printf("Starting scan on IP:%s from port %s to %s\n\n", argv[1],argv[2],argv[3]);
 
+    freopen("output.txt","w",stdout); // Write results i.e. port numbers and messages to output.txt 
 
-	// Creating ip header
-	struct ipheader *iph = (struct ipheader *) datagram;
-     
-    iph->iph_tos = 0; //Type of service
-    iph->iph_len = sizeof (struct ipheader) + sizeof (struct udpheader) + strlen(data);  //Total length
-    iph->iph_ident = htonl(11); //Identification
-    iph->iph_offset = 0; //Fragment offset
-    iph->iph_ttl = 255; //Time to live
-    iph->iph_protocol = IPPROTO_UDP; //Protocol
-    
-    iph->iph_source = inet_addr (source_ip); //Source address
-    // iph->iph_dest = inet_addr ("130.208.243.61"); //Destination address
-	iph->iph_dest = inet_addr (argv[1]); //Destination address
+    int udp_socket;                // create socket 
+    char buffer_tx[64];            // transmission buffer
+    int len_buffer_tx;             // length of transmission message   
+    short int port_lo = atoi(argv[2]);
+    short int port_hi = atoi(argv[3]);
+    struct sockaddr_in destaddr;   // Destination address
 
-
-   //Ip checksum
-	iph->iph_csum = csum ((unsigned short *) datagram, iph->iph_len);
-
-    // UDP header
-    int size_udph = strlen(datagram) + sizeof (struct ipheader);
-
-    udph->udph_srcport = htons(0);
-    udph->udph_destport = htons(4000);
-    udph->udph_len = size_udph + strlen(data);
-    udph->udph_csum = 0;
-
-    
-    //Now the UDP checksum
-	psh.source_address = inet_addr( source_ip );
-	psh.dest_address = sin.sin_addr.s_addr;
-	psh.placeholder = 0;
-	psh.protocol = IPPROTO_UDP;
-	psh.tcp_length = htons(size_udph + strlen(data) );
-	
-	int psize = sizeof(struct pseudo_header) + size_udph + strlen(data);
-	pseudogram = (char *) malloc(psize);
-	
-	memcpy(pseudogram , (char*) &psh , sizeof (struct pseudo_header));
-	memcpy(pseudogram + sizeof(struct pseudo_header) , udph , size_udph + strlen(data));
-	
-	udph->udph_csum = csum( (unsigned short*) pseudogram , psize);
-	
-
-    //Create a raw UDP socket
-    int s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); 
-	
-	if(s== -1)
-	{
-		//socket creation failed, may be because of non-root privileges
-		perror("Failed to create socket");
-		exit(1);
-	}
-
-	
-	// http://www.microhowto.info/howto/listen_for_and_receive_udp_datagrams_in_c.html
-	char buffer[512];
+    // initializing receive buffer 
+    char buffer_rx[1300];
 	struct sockaddr_storage src_addr;
 
 	struct iovec iov[1];
-	iov[0].iov_base=buffer;
-	iov[0].iov_len=sizeof(buffer);
+	iov[0].iov_base=buffer_rx;
+	iov[0].iov_len=sizeof(buffer_rx);
 
 	struct msghdr message;
 	message.msg_name=&src_addr;
@@ -183,51 +51,157 @@ int main (int argc, char* argv[]) {
 	message.msg_control=0;
 	message.msg_controllen=0;
 
+    int n_reruns = 10;                  // number of reruns of the port range
 
+    int size = 4; 
+    int* port_numbers = new int[size];  // initialize array containing open ports numbers
+    int n_ports = 0;                    // number of current found open ports
+
+
+
+    strcpy(buffer_tx, "Are you open?"); // message to send
+    len_buffer_tx = strlen(buffer_tx) +1;   // length of message +1, to not send the whole buffer
+  
+
+    // socket setup
+    udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (udp_socket<0){
+        perror("ERROR - Failed to create socket\n");
+        exit(0);
+    }
     // initializing timeout for socket
 	// https://stackoverflow.com/questions/13547721/udp-socket-set-timeout
 	struct timeval tv;
-	tv.tv_sec = 2;
-	tv.tv_usec = 0;	
-	if (setsockopt (s, SOL_SOCKET, SO_RCVTIMEO ,&tv,sizeof(tv)) < 0)
+	tv.tv_sec = 0;
+	tv.tv_usec = 1000;	  // 0.001s timeout 
+	if (setsockopt (udp_socket, SOL_SOCKET, SO_RCVTIMEO ,&tv,sizeof(tv)) < 0)   // set timelimit on each socket
 	{
 		perror("Error socket setup options");
 		exit(0);
 	}
 
-	int i = atoi(argv[2]);
-	int i_stop = atoi(argv[3])+1;
-	while (i<i_stop){
-		sin.sin_port = htons(i);
+    // setting destination address
+    destaddr.sin_family = AF_INET;
+    destaddr.sin_addr.s_addr = inet_addr (argv[1]);  // inet_addr ("130.208.243.61");  
+    
 
-		//Send the packet
-		if (sendto (s, data, udph->udph_len , 0, (struct sockaddr *) &sin, sizeof (sin)) < 0)
-		{
-			perror("sendto failed");
-		}
-		//Data send successfully
-		else
-		{
-			printf ("Packet Sent to port %d \n" , i); 
-		}
+    for (int i = 0; i<n_reruns; i++){
+        // printf("run: %d\n",i);
+        for(int port = port_lo; port <= port_hi; port++){
 
-		ssize_t count=recvmsg(s,&message,0);
-		
-		if (count==-1) {
-			printf("%s",strerror(errno));
-		} else if (message.msg_flags&MSG_TRUNC) {
-			printf("datagram too large for buffer: truncated");
-		} else {
-			printf("%s",buffer);
-	
-		}
+            destaddr.sin_port = htons(port); // update port number
 
-		printf("\n\n");
+            if (sendto (udp_socket, buffer_tx, len_buffer_tx , 0, (struct sockaddr *) &destaddr, sizeof (destaddr)) < 0){
+                perror("Error - Sendto failed");
+            }
+            // else{
+            //     printf("Package sent to %d\n",port);
+            // }      
+            
+            ssize_t count=recvmsg(udp_socket,&message,0);  
+            
+            
+            if (count==-1) {
+                // timout or package drop
+            } else if (message.msg_flags&MSG_TRUNC) {
+                printf("datagram too large for buffer: truncated\n");
+            } else {
+                // printf("%d -- %s\n",port,buffer_rx);
+                
+                if(not_in_arr(port_numbers, n_ports, port)){    // if port not already in port_numbers
+                    
+                    if (n_ports == size){                      // resize array if to big
+                        arr_resize(*& port_numbers);
+                        size *= 2; 
+                    }
 
-		i++; 
+                    port_numbers[n_ports] = port;              // add port to port_numbers
+                    n_ports++;
 
-	} 
+                    //printf("%d -- %s\n",port,buffer_rx);
+                    printf("\nport %d\n",port);
+                    print_msg(buffer_rx,count);                 
+                }
 
 
-	return 0;
+            }
+
+            
+        
+         }
+
+    }
+    // print_arr(port_numbers, n_ports);
+
+
+    fclose (stdout); // close file
+
+    delete [] port_numbers; // free memory 
+    port_numbers = NULL; 
+
+
+    freopen ("/dev/tty", "a", stdout);  // send printing back to console
+
+    printf("Scan finished! \nResults can be seen in current working directory in output.txt\n");
+
+    
+    
+
 }
+
+
+void arr_resize(int* & arr ){
+    // double size of arr
+    int* resize = new int[sizeof(arr)*2];
+
+    for (unsigned int i = 0; i<sizeof(arr); i++){
+        resize[i] = arr[i];
+    }
+
+    delete[] arr;  // delete old array
+    arr = resize;  
+}
+
+bool not_in_arr(int* arr, int n_foundports ,int port_poss){
+    // returns false if port_poss is in arr
+    for (int i = 0; i<n_foundports; i++){
+        if (arr[i] == port_poss){
+            return false;
+        }
+    }
+    return true;
+}
+
+void print_arr(int * arr, int num_ports){
+    // prints out arr
+    for (int i = 0; i < num_ports; i++){
+        std::cout<<arr[i]<<" ";
+    }
+    std::cout<<"\n";
+}
+
+void print_msg(char* buf, unsigned int msg_size){
+    // prints out the messages from the buf
+    for (unsigned int i = 0; i<msg_size; i++){
+        std::cout<<buf[i];
+    }
+    std::cout<<"\n";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
