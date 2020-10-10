708227cea0499534070028e5a690739353e16d20
jonasmark <jonasmar14@gmail.com>
2020-10-10 12:33:18 +0000

    bingo

--- Makefile
+++
@@ -1,6 +0,0 @@
-default: 
-	g++ -Wall -std=c++11 scanner.cpp -o scanner
-	g++ -Wall -std=c++11 part2-solver.cpp -o solver
-	g++ -Wall -std=c++11 part3.cpp -o knocker
-	hostname -I > IPv4address.txt
-        
---
+++ client.cpp
@@ -0,0 +1,134 @@
+//
+// Simple chat client for TSAM-409
+//
+// Command line: ./chat_client 4000 
+//
+// Author: Jacky Mallett (jacky@ru.is)
+//
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <thread>
+
+#include <iostream>
+#include <sstream>
+#include <thread>
+#include <map>
+
+// Threaded function for handling responss from server
+
+void listenServer(int serverSocket)
+{
+    int nread;                                  // Bytes read from socket
+    char buffer[1025];                          // Buffer for reading input
+
+    while(true)
+    {
+       memset(buffer, 0, sizeof(buffer));
+       nread = read(serverSocket, buffer, sizeof(buffer));
+
+       if(nread == 0)                      // Server has dropped us
+       {
+          printf("Over and Out\n");
+          exit(0);
+       }
+       else if(nread > 0)
+       {
+          printf("%s\n", buffer);
+       }
+       printf("here\n");
+    }
+}
+
+int main(int argc, char* argv[])
+{
+   struct addrinfo hints, *svr;              // Network host entry for server
+   struct sockaddr_in serv_addr;           // Socket address for server
+   int serverSocket;                         // Socket used for server 
+   int nwrite;                               // No. bytes written to server
+   char buffer[1025];                        // buffer for writing to server
+   bool finished;                   
+   int set = 1;                              // Toggle for setsockopt
+
+   const char* port_char = "5000";
+
+   if(argc != 2)
+   {
+        // printf("Usage: chat_client <ip  port>\n");
+        printf("Hard coded to connect to port 5000\n");
+        printf("Ctrl-C to terminate\n");
+        exit(0);
+   }
+
+   hints.ai_family   = AF_INET;            // IPv4 only addresses
+   hints.ai_socktype = SOCK_STREAM;
+
+   memset(&hints,   0, sizeof(hints));
+
+   if(getaddrinfo(argv[1], port_char, &hints, &svr) != 0)
+   {
+       perror("getaddrinfo failed: ");
+       exit(0);
+   }
+
+   struct hostent *server;
+   server = gethostbyname(argv[1]);                      // finnur automatic IP tölu ef sett er inn "skel.ru.is" en annars bara bein IP tala
+
+   bzero((char *) &serv_addr, sizeof(serv_addr));
+   serv_addr.sin_family = AF_INET;
+   bcopy((char *)server->h_addr,
+      (char *)&serv_addr.sin_addr.s_addr,
+      server->h_length);
+   serv_addr.sin_port = htons(atoi(port_char));
+
+   serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+   // Turn on SO_REUSEADDR to allow socket to be quickly reused after 
+   // program exit.
+
+   if(setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &set, sizeof(set)) < 0)
+   {
+       printf("Failed to set SO_REUSEADDR for port %s\n", port_char);
+       perror("setsockopt failed: ");
+   }
+
+   
+   if(connect(serverSocket, (struct sockaddr *)&serv_addr, sizeof(serv_addr) )< 0)
+   {
+       printf("Failed to open socket to server: %s\n", argv[1]);
+       perror("Connect failed: ");
+       exit(0);
+   }
+
+    // Listen and print replies from server
+   std::thread serverThread(listenServer, serverSocket);
+
+   finished = false;
+   while(!finished)
+   {
+       bzero(buffer, sizeof(buffer));
+
+       fgets(buffer, sizeof(buffer), stdin);
+
+       nwrite = send(serverSocket, buffer, strlen(buffer),0);
+
+       if(nwrite  == -1)
+       {
+           perror("send() to server failed: ");
+           finished = true;
+       }
+
+   }
+}
